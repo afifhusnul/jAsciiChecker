a94aa201acac31dd7adabee0bc5207e1b74433c6
NUSNAFIF <NUSNAFIF@MKT_N03302.DiGiCR.digi.com.my>
2012-05-25 16:28:40 +0800

    Update by Afif 25May2012

--- src/com/swingmvc/jasciichecker/Main.java
+++ src/com/swingmvc/jasciichecker/Main.java
@@ -5,6 +5,7 @@
 package com.swingmvc.jasciichecker;
 
 import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -17,11 +18,13 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         // TODO code application logic here
         try {
-            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            //UIManager.setLookAndFeel(new NimbusLookAndFeel());
             java.awt.EventQueue.invokeLater(new Runnable() {
+                @Override
                 public void run() {
                     new jAsciiChecker().setVisible(true);
                 }
